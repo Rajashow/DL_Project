352752c4deb28b912206274341d1902444ae956e
Rajashow <rajashowparajuli@gmail.com>
2020-05-10 16:36:08 -0400

    Update species.py

--- species.py
+++ species.py
@@ -1,3 +1,5 @@
+from train import Train
+from wann import wann
 from ulti import get_pop_rank, get_split
 import bisect
 
@@ -7,7 +9,6 @@ class Species():
         self.max_fit = 0
         self.mean_fit = 0
         self.species_list = []
-        self.parent = []
 
     def reap(self, p):
         numb_to_reap = int(len(self.species_list)*p)
@@ -22,4 +23,32 @@ class Species():
 
         bounds = get_split(fitness)
         split = bisect.bisect_right(fitness, bounds[1])
-        new_Species = self.species_list[:split+1]
+        new_Species = Species()
+
+        new_Species.species_list = self.species_list[:split+1]
+        self.species_list = self.species_list[split+1:]
+
+        new_Species.max_fit = max(fitness[:split+1])
+        new_Species.mean_fit = sum(fitness[:split+1])/len(fitness[:split+1])
+
+        self.max_fit = max(fitness[split+1:])
+        self.mean_fit = sum(fitness[split+1:])/len(fitness[split+1:])
+
+        return self, new_Species
+
+
+if __name__ == "__main__":
+    wann_class = wann
+
+    hyper_params = {"p_weighed_rank": .5, "w": -2, "%_reap": .5}
+    class_args = {"input_dim": (784), "num_classes": 300}
+    trainer = Train(wann_class, class_args, 100, hyper_params)
+
+    for i in range(len(trainer.pop)):
+        if i > 50:
+            trainer.pop[i].fitness = i*20
+        else:
+            trainer.pop[i].fitness = i
+    sp = Species()
+    sp.species_list = trainer.pop
+    sp.speciete()
