2eaf5518f094d9fea1ac6d9251448fe67f255c99
kvathupo <kalinda619@gmail.com>
2020-05-09 22:00:41 -0400

    convert float32

--- sketchdataset.py
+++ sketchdataset.py
@@ -47,10 +47,10 @@ class SketchDataSet(Dataset):
         if self.is_train:
             for i in range(0, self.num_class):
                 if ind - self.fimgsize[i] < 0 and i == 0:
-                    img_dat = np.load(self.fnames[0])[ind].reshape(28, 28)
+                    img_dat = np.load(self.fnames[0])[ind].reshape(1, 28, 28).astype(np.float32)
                     return torch.from_numpy(img_dat), i
                 elif ind - self.fimgsize[i] < 0:
-                    img_dat = np.load(self.fnames[i])[ind - self.fimgsize[i-1]].reshape(28, 28)
+                    img_dat = np.load(self.fnames[i])[ind - self.fimgsize[i-1]].reshape(1, 28, 28).astype(np.float32)
                     return torch.from_numpy(img_dat), i
         else:
             for i in range(0, self.num_class):
@@ -58,12 +58,12 @@ class SketchDataSet(Dataset):
                     np_dat = np.load(self.fnames[0])
                     ind_offset = int(len(np_dat)*0.8)
 
-                    img_dat = np_dat[ind + ind_offset].reshape(28, 28)
+                    img_dat = np_dat[ind + ind_offset].reshape(1, 28, 28).astype(np.float32)
                     return torch.from_numpy(img_dat), i
                 elif ind - self.fimgsize[i] < 0:
                     np_dat = np.load(self.fnames[0])
                     ind_offset = int(len(np_dat)*0.8)
 
-                    img_dat = np_dat[ind + ind_offset - self.fimgsize[i-1]].reshape(28, 28)
+                    img_dat = np_dat[ind + ind_offset - self.fimgsize[i-1]].reshape(1, 28, 28).astype(np.float32)
                     return torch.from_numpy(img_dat), i
         return None, None
