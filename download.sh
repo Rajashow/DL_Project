8d0af1afb2c01ad5d5c002a9a53fb7ba01557608
Rajashow <rajashowparajuli@gmail.com>
2020-05-09 22:16:51 -0400

    Create download.sh

---
+++ download.sh
@@ -0,0 +1,11 @@
+
+
+
+mkdir data
+
+gsutil -m cp gs://quickdraw_dataset/full/numpy_bitmap/airplane.npy data/
+gsutil -m cp gs://quickdraw_dataset/full/numpy_bitmap/ant.npy data/
+gsutil -m cp gs://quickdraw_dataset/full/numpy_bitmap/arm.npy data/
+gsutil -m cp gs://quickdraw_dataset/full/numpy_bitmap/cat.npy data/
+gsutil -m cp gs://quickdraw_dataset/full/numpy_bitmap/dog.npy data/
+gsutil -m cp gs://quickdraw_dataset/full/numpy_bitmap/bee.npy data/
