5829bcb374bbfa6d534e12d9b81230b055589a94
kvathupo <kalinda619@gmail.com>
2020-05-09 22:00:27 -0400

    tweaked

--- resnetbasic.py
+++ resnetbasic.py
@@ -7,22 +7,22 @@ class BasicRes(nn.Module):
     def __init__(self):
         super(BasicRes, self).__init__()
         self.block1 = nn.Sequential(
-            nn.Dropout(p=0.2)
-            nn.Conv2d(3, 1, 7)
+            nn.Dropout(p=0.2),
+            nn.Conv2d(1, 1, 7)
         )
         self.res_block = nn.Sequential(
-            self.res_unit1 = nn.Conv2d(1, 1, 3, padding=1),  # D_in = (W_in, H_in, C_in) = D_out = (W_out, H_out, C_out)
-            self.res_unit2 = nn.Conv2d(1, 1, 3, padding=1),  # D_in = D_out
-            self.res_unit3 = nn.Conv2d(1, 2, 3, stride=2, padding=1),    # D_out = (W_in/2, H_in/2, 2)
-            self.res_unit4 = nn.Conv2d(2, 2, 3, padding=1),  # D_in = (W_in, H_in, 2) = D_out
-            self.res_unit5 = nn.Conv2d(2, 2, 3, padding=1),  # D_in = D_out
-            self.res_unit6 = nn.Conv2d(2, 4, 3, stride=2, padding=1),    # D_out = (W_in/2, H_in/2, 4)
-            self.res_unit7 = nn.Conv2d(4, 4, 3, padding=1),  # D_in = D_out
-            self.res_unit8 = nn.Conv2d(4, 4, 3, padding=1),  # D_in = D_out
-            self.res_unit9 = nn.Conv2d(4, 8, 3, stride=2, padding=1),    # D_out = (W_in/2, H_in/2, 8) 
-            self.res_unit10 = nn.Conv2d(8, 8, 3, padding=1),  # D_in = D_out
-            self.res_unit11 = nn.Conv2d(8, 8, 3, padding=1),  # D_in = D_out
-            self.res_unit12 = nn.Conv2d(8, 16, 3, stride=2, padding=1)    # D_out = (W_in/2, H_in/2, 12) 
+            nn.Conv2d(1, 1, 3, padding=1),  # D_in = (W_in, H_in, C_in) = D_out = (W_out, H_out, C_out)
+            nn.Conv2d(1, 1, 3, padding=1),  # D_in = D_out
+            nn.Conv2d(1, 2, 3, stride=2, padding=1),    # D_out = (W_in/2, H_in/2, 2)
+            nn.Conv2d(2, 2, 3, padding=1),  # D_in = (W_in, H_in, 2) = D_out
+            nn.Conv2d(2, 2, 3, padding=1),  # D_in = D_out
+            nn.Conv2d(2, 4, 3, stride=2, padding=1),    # D_out = (W_in/2, H_in/2, 4)
+            nn.Conv2d(4, 4, 3, padding=1),  # D_in = D_out
+            nn.Conv2d(4, 4, 3, padding=1),  # D_in = D_out
+            nn.Conv2d(4, 8, 3, stride=2, padding=1),    # D_out = (W_in/2, H_in/2, 8) 
+            nn.Conv2d(8, 8, 3, padding=1),  # D_in = D_out
+            nn.Conv2d(8, 8, 3, padding=1),  # D_in = D_out
+            nn.Conv2d(8, 16, 3, stride=2, padding=1)    # D_out = (W_in/2, H_in/2, 12) 
         )
 
     def forward(self, x):
