88f1342ae31a354daa4d67a567ea200bfc9248bb
Davin Park <dpark3542@gmail.com>
2020-05-12 22:27:47 -0400

    Updated download.sh.

--- download.sh
+++ download.sh
@@ -1,11 +1,20 @@
-
-
-
 mkdir data
 
+gsutil -m cp "gs://quickdraw_dataset/full/numpy_bitmap/The Eiffel Tower.npy" data/
+gsutil -m cp "gs://quickdraw_dataset/full/numpy_bitmap/The Mona Lisa.npy" data/
+gsutil -m cp "gs://quickdraw_dataset/full/numpy_bitmap/aircraft carrier.npy" data/
 gsutil -m cp gs://quickdraw_dataset/full/numpy_bitmap/airplane.npy data/
+gsutil -m cp "gs://quickdraw_dataset/full/numpy_bitmap/alarm clock.npy" data/
+gsutil -m cp gs://quickdraw_dataset/full/numpy_bitmap/ambulance.npy data/
+gsutil -m cp gs://quickdraw_dataset/full/numpy_bitmap/angel.npy data/
+gsutil -m cp "gs://quickdraw_dataset/full/numpy_bitmap/animal migration.npy" data/
 gsutil -m cp gs://quickdraw_dataset/full/numpy_bitmap/ant.npy data/
-gsutil -m cp gs://quickdraw_dataset/full/numpy_bitmap/arm.npy data/
-gsutil -m cp gs://quickdraw_dataset/full/numpy_bitmap/cat.npy data/
-gsutil -m cp gs://quickdraw_dataset/full/numpy_bitmap/dog.npy data/
-gsutil -m cp gs://quickdraw_dataset/full/numpy_bitmap/bee.npy data/
+gsutil -m cp gs://quickdraw_dataset/full/numpy_bitmap/anvil.npy data/
+gsutil -m cp gs://quickdraw_dataset/full/numpy_bitmap/apple.npy data/
+gsutil -m cp gs://quickdraw_dataset/full/numpy_bitmap/asparagus.npy data/
+gsutil -m cp gs://quickdraw_dataset/full/numpy_bitmap/banana.npy data/
+gsutil -m cp gs://quickdraw_dataset/full/numpy_bitmap/barn.npy data/
+gsutil -m cp gs://quickdraw_dataset/full/numpy_bitmap/bathtub.npy data/
+gsutil -m cp gs://quickdraw_dataset/full/numpy_bitmap/bear.npy data/
+gsutil -m cp gs://quickdraw_dataset/full/numpy_bitmap/binoculars.npy data/
+gsutil -m cp "gs://quickdraw_dataset/full/numpy_bitmap/birthday cake.npy" data/
